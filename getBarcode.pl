7123ccfae11b2a8c45b44e8634f2671b28511d05
yuanjingnan <62393653+yuanjingnan@users.noreply.github.com>
2023-07-26 22:00:47 +0800

    Add files via upload

---
+++ getBarcode.pl
@@ -0,0 +1,10 @@
+open IB,"gzip -dc $ARGV[0]|";
+while(<IB>){
+	chomp;
+	next unless ($_ =~ /^@/);
+	my($reads,$bar,$umi) = split /\s+/;
+    my($reads1,$x_raw,$y_raw) = split /\|/,$reads;
+    my($x1,$x2,$x) = split /:/,$x_raw;
+    my($y1,$y2,$y) = split /:/,$y_raw;
+    print "$reads1|||CB:Z:$x\_$y|||UR:Z:$umi\n";
+}
