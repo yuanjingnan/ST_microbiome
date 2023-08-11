3220dfa8d6faa976acd0106ff1dd429b2b4c3a7b
yuanjingnan <62393653+yuanjingnan@users.noreply.github.com>
2023-08-12 01:30:32 +0800

    Add files via upload

--- select_CH.pl
+++ select_CH.pl
@@ -1,28 +1,34 @@
 #!/usr/bin/perl
-
 use strict;
 use warnings;
 
-# 读取表格a
+
+# 打开表格文件，读取文件内容，生成哈希表
+open my $fh2, "<", $ARGV[1] or die "Can't open file: $!";
+my $query_id = $ARGV[2];
+
+my %table;
+while (my $line = <$fh2>) {
+    chomp $line;
+    my ($sample, $species) = split /\t/, $line; 
+    if ($sample eq $query_id) {
+	    $species =~ s/ /_/g;
+            $table{$species}=1;
+        }
+}
+close $fh2;
+
 open my $fh1, '<', $ARGV[0] or die "无法打开文件：$!";
 my @table_a = <$fh1>;
 close ($fh1);
 
-# 读取表格b
-open my $fh2, "<", $ARGV[1] or die "Can't open file: $!";
-my %table_b;
-while (<$fh2>) {
-    chomp;
-    $table_b{$_} = 1;
-}
-close ($fh2);
-
-# 提取表格a中与表格b相同值的行
 foreach my $row (@table_a) {
     chomp $row;
     my @data = split /\t/, $row;
-    my $key = (split /_/, $data[2])[0];
-    if ($table_b{$key}) {
+    my $key = $data[2];
+    $key =~ s/ /_/g;
+    if (exists $table{$key}) {
         print $row."\n";
     }
 }
+
